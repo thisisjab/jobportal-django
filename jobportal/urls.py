234127d1a430e5a16965ce56fa9b0164e5657197
thisisjab <heythisisjab@gmail.com>
2023-06-09 23:31:31 +0330

    Register authentication urls

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]

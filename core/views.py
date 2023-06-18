ae2edf338a163e3a4e0bbd3b43da166df08dc8cb
thisisjab <heythisisjab@gmail.com>
2023-06-18 20:58:39 +0330

    Register core urls and create signup view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from . import forms
 
-# Create your views here.
+
+class SignUpView(CreateView):
+    form_class = forms.UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        return super(SignUpView,self).form_valid(form)

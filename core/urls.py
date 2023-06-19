ef07204e0aeb8b314d774ab1b489e5ac24bf7f56
thisisjab <heythisisjab@gmail.com>
2023-06-19 15:07:33 +0330

    Config urls related to email confirmation

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup')
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('confirm_your_email', views.ConfirmEmailView.as_view(), name='confirm_email'),
+    path('activate/<uidb64>/<token>', views.ActivateView.as_view(), name='activate')
 ]

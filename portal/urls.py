87c4450bc27f642577769e140a62fc607beb6d51
thisisjab <heythisisjab@gmail.com>
2023-06-28 20:12:11 +0330

    Add JobCandidate create view

--- portal/urls.py
+++ portal/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'portal'
 urlpatterns = [
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/jobcandidate/new', views.JobCandidateProfileCreateView.as_view(), name='add_jobcandidate'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,24 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from . import forms
+from . import models
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'portal/profile.html'
+
+
+class JobCandidateProfileCreateView(LoginRequiredMixin, FormView):
+    form_class = forms.JobCandidateProfileForm
+    success_url = reverse_lazy('portal:profile')
+    template_name = 'portal/jobcandidate_profile_form.html'
+
+    def form_valid(self, form):
+        models.JobCandidate(
+            birth_date=form.cleaned_data['birth_date'].isoformat() if form.cleaned_data.get('birth_date', None) else None,
+            short_bio=form.cleaned_data['short_bio'],
+            description=form.cleaned_data['description'],
+            user=self.request.user
+        ).save()
+        return super(JobCandidateProfileCreateView, self).form_valid(form)

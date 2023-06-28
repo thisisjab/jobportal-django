ff43b6ae7fae2cd8ea406d061cd842c1606a1125
thisisjab <heythisisjab@gmail.com>
2023-06-28 18:58:59 +0330

    Add JobCandidate profile form

---
+++ portal/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from . import models
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class JobCandidateProfileForm(forms.ModelForm):
+    
+    class Meta:
+        model = models.JobCandidate
+        fields = ['birth_date', 'short_bio', 'description']
+        widgets = {
+            'birth_date': DateInput()
+        }
